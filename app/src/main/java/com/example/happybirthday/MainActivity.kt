8f2099b5da1f58c2c429dc0f501253bb4176d361
tarantulDeveloper <tarantuldeveloper@gmail.com>
2023-02-04 18:09:32 +0600

    some refactoring

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,18 +3,38 @@ package com.example.happybirthday
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton : Button = findViewById(R.id.button)
-        rollButton.setOnClickListener{
-            val resultText : TextView = findViewById(R.id.textView)
-            resultText.text = "6"
-        }
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { spinCube() }
+
+    }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun spinCube() {
+        // Create new Dice object with 6 sides and roll it
+        val cube = Cube(6)
+        val cubeSpin = cube.spin()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = cubeSpin.toString()
+    }
+}
+
+class Cube(private val numSides: Int) {
+    fun spin(): Int {
+        return (1..numSides).random()
     }
 }
